9947fb783783571d5e294deb332a7853bbeb15c6
Siclol <marko.milky@gmail.com>
2019-07-22 10:27:59 +0200

    chore(house): setting upp house component and styles

--- src/components/House.svelte
+++ src/components/House.svelte
@@ -4,12 +4,7 @@
 <style>
 
 .grid {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-around;
-  width: 100%;
   max-width: 1200px;
-  margin: 0 auto;
 }
 
 </style>
@@ -17,7 +12,19 @@
 
 
 
-<div class="grid">
-<h1 class="headline relative c-dark ta-left m-y-l">House</h1>
-
+<div class="grid m-y-l">
+    <div class="col-12 ta-left">
+        <h1 class="headline relative c-dark p-b-s">House</h1>
+    </div>
+    <div class="col-4">
+        <img src="assets/images/blue-doors.jpg" alt="House exterior">
+    </div>
+    <div class="col-8 flex fd-col ai-end">
+        <h5 class="c-dark m-t-s w-full ta-left">AWESOME PLACE TO ENJOY</h5>
+        <p class="font-secondary m-y-s p-a-m w-full ta-left">With its spectacular views of the sea and islands, the mountain and blue Mediterranean skies, this newly renovated house is a combination of traditional Dalmatian architecture and modern interventions, built with respect for and appreciation of the tradition, thus resulting in a new construction having that charming old Dalmatian feel. The exterior stones in warm light beige tone and blue window shutters are in complete harmony with the blue sea, rocky mountain and the surrounding Mediterranean vegetation.</p>
+        <a href='/' class="c-accent"><i>...Read more</i></a>
+        <div class="gallery m-t-m">
+            <img src="assets/images/exterior.jpg" alt="">
+        </div>
+    </div>
 </div>
--- src/components/Location.svelte
+++ src/components/Location.svelte
@@ -20,7 +20,7 @@
 <section id="location" class="m-y-l">
     <div class="grid">
         <div class="col-6">
-            <h1 class="c-dark relative p-b-m headline">LOCATION</h1>
+            <h1 class="c-dark relative p-b-s headline">LOCATION</h1>
             <p class="ta-justify m-t-m font-secondary">Villa Jatica is located in the Southern Dalmatia, on the slopes of Biokovo Mountain. Perched on the heights above the village of Zaostrog, the villa offers fantastic view of the Adriatic Sea, the tip of Pelješac peninsula, and the islands of Korčula and Hvar. It is a perfect quiet getaway to leave the world behind and truly unwind in wilderness and natural beauty of this remote destination, yet to pamper yourself in complete comfort and luxury the house offers.</p>
         </div>
         <div class="col-5 ta-right">
