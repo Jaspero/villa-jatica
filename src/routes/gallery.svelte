fc0cbdc12a00efd868d48a0cada9810c1328f502
Siclol <marko.milky@gmail.com>
2019-08-07 15:31:56 +0200

    chore(gallery)setting up gallery page

--- src/routes/gallery.svelte
+++ src/routes/gallery.svelte
@@ -1,4 +1,63 @@
+
+<script>
+
+</script>
+
+
+
+<style>
+
+.albums {
+    width: 400px;
+    position: fixed;
+    left: 0;
+    top: 55px;
+    height: 100%;
+    background: #cccccc;
+}
+
+.exterior {
+    background-image: url("../../assets/images/exterior-1.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+}
+
+.album {
+background: white;
+height: 150px;
+width: 100%;
+}
+
+
+</style>
+
+
 <svelte:head>
 	<title>Gallery</title>
 </svelte:head>
-<h1>wow this is awesome gallery</h1>
+
+
+<nav class="navbar p-y-s flex bg-warn ai-center z-index">
+    <a href="#home" class="flex ai-center">
+        <img src="assets/icons/left.svg" alt="back">
+        <p class="font-secondary m-l-s">Back</p>
+    </a>
+    <p class="c-accent m-l-l"><i>Beautifull galleries of our villa Jatica, from interior to exterior!</i></p>
+</nav>
+
+<div class="flex">
+    <div class="albums flex fw-wrap">
+    <div class="album col-6">
+        <div class="exterior">
+            <p><i>House exterior</i></p>
+        </div>
+    </div>
+    <div class="col-6 interior"></div>
+    <div class="col-6 village"></div>
+    <div class="col-6 house"></div>
+    <div class="col-6 nature"></div>
+    <div class="col-6 photos"></div>
+
+    </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,6 +11,17 @@
 
     let segment
 
+	function activeHeader() {
+	const headerEl = document.querySelector('#header');
+	const activeEl = headerEl.pageYOffset;
+
+   if (activeEl.pageYOffset > active) {
+     headerEl.classList.add("active");
+   } else {
+     headerEl.classList.remove("active");
+   }
+
+}
 
 </script>
 
