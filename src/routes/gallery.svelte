1ac1629d87aee977c0533c8bbfa4c8ddbea465f0
Siclol <marko.milky@gmail.com>
2019-08-13 13:49:50 +0200

    chore(gallery) added tabs for gallery

--- src/routes/gallery.svelte
+++ src/routes/gallery.svelte
@@ -95,15 +95,6 @@
 
 <div class="flex gallery-container">
     <div class="albums flex fw-wrap ac-flex col-4">
-    <div class="grid">
-              <div class="gg-tabs-buttons">
-                <button class="gg-tabs-button" class:active="{tab === 0}" on:click={() => tab = 0}>High-throughput analyses</button>
-                <button class="gg-tabs-button" class:active="{tab === 1}" on:click={() => tab = 1}>In depth glycoprofiling</button>
-                <button class="gg-tabs-button" class:active="{tab === 2}" on:click={() => tab = 2}>Study design</button>
-                <button class="gg-tabs-button" class:active="{tab === 3}" on:click={() => tab = 3}>Data analysis</button>
-                <button class="gg-tabs-button" class:active="{tab === 4}" on:click={() => tab = 4}>Joint project development</button>
-              </div>
-</div>
         <div class="col-6">
             <button class="h-full w-full small-gallery" class:active="{tab === 0}" on:click={() => tab = 0}>
             <div class="exterior"></div>
@@ -161,22 +152,51 @@
     </div>
      <div class="col-12">
                         {#if tab === 0}
-                            <p>Immunoglobulin G (IgG) N-glycosylation analysis (analysis of total released N-glycans by UPLC-FLR and subclass-specific Fc N-glycopeptide analysis by LC-MS)</p>
-                            <p>Plasma N-glycosylation analysis (analysis of total released N-glycans by UPLC-FLR)</p>
+                        <div class="exterior-container">
+                            <div class="grid">
+                                <div class="col-3 exterior-image">
+                                </div>
+                            </div>
+                        </div>
                         {:else if tab === 1}
-                            <p>Analysis of released N-glycans</p>
-                            <p>Site-specific N-glycosylation analysis</p>
-                            <p>N-glycosylation site occupancy analysis</p>
+                        <div class="interior-container">
+                            <div class="grid">
+                                <div class="col-3 exterior-image">
+                                </div>
+                            </div>
+                        </div>
                         {:else if tab === 2}
-                            <p>Genos offers consultancies in study design, under the full or partial control of the experiment, for different fields of biomedicine. The mixture of expertise from Genos employees (biologists, pharmacist, chemists, statisticians and mathematicians) ensures the quality of the study design.</p>
+                        <div class="interior-container">
+                            <div class="grid">
+                                <div class="col-3">
+                                    <img src="assets/images/village.jpg" alt="exterior" class="w-full">
+                                </div>
+                            </div>
+                        </div>
                         {:else if tab === 3}
-                            <p>Data analysis is an important aspect of the workflow Genos provides to the public. Since pure numbers, without thoughtful and meaningful explanation, are usually of little interest Genos invested significantly to its employee’s competence in statistics and computer science. Therefore, Genos now offers a wide range of statistical techniques from basic experimental methods of normalization or batch correction to standard topics like statistical inference or machine/statistical learning.</p>
+                        <div class="details-container">
+                            <div class="grid">
+                                <div class="col-3">
+                                    <img src="assets/images/details.jpg" alt="exterior" class="w-full">
+                                </div>
+                            </div>
+                        </div>
                         {:else if tab === 4}
-                            <h6>Cooperation and joint project development</h6>
-                            <p>Genos is also an active research organization. On the basis of the recommendation of the National Science Council, the Ministry of Science, Education and Sports of the Republic of Croatia issued a permit that allowed Genos to formally register as a research organization in 2007. Genos currently participates in six FP7 projects (GlycoBioM, HighGlycan, IBD-BIOM, MIMOmics, Pain-OMICS and HTP-GlycoMet).</p>
-                            <p>We are constantly in search of potential collaborative projects where we can contribute with our expertise in high-throughput glycomics and extensive experience of participation in large FP7 and H2020 consortia.</p>
+                        <div class="nature-container">
+                            <div class="grid">
+                                <div class="col-3">
+                                    <img src="assets/images/interior.jpg" alt="exterior" class="w-full">
+                                </div>
+                            </div>
+                        </div>
                             {:else if tab === 5}
-                            <h1 class="c-dark">alooooo</h1>
-                        {/if}
+                            <div class="guest-container">
+                                <div class="grid">
+                                    <div class="col-3">
+                                        <img src="assets/images/interior.jpg" alt="exterior" class="w-full">
+                                    </div>
+                                </div>
+                            </div>
+                            {/if}
                     </div>
 </div>
