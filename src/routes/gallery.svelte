9b66e76ad48b12c514d62211ad9e7f820fad1dcc
Siclol <marko.milky@gmail.com>
2019-08-14 09:51:43 +0200

    chore(gallery) working on gallery mobile menu

--- src/routes/gallery.svelte
+++ src/routes/gallery.svelte
@@ -3,6 +3,12 @@
     import { fade } from 'svelte/transition';
 
     export let tab = 0;
+
+        function albumsMenu() {
+          const albumsEl = document.querySelector('.albums');
+          albumsEl.classList.toggle('active-albums');
+        }
+
 </script>
 
 
@@ -89,8 +95,9 @@
 </nav>
 
 <div class="flex gallery-container">
-    <div class="albums flex fw-wrap ac-flex col-4">
-        <div class="col-6">
+
+    <div class="albums flex fw-wrap ac-flex col-4 col-s-12 hide-xs">
+        <div class="col-6 col-s-12">
             <div class="h-full w-full " class:active="{tab === 0}" on:click={() => tab = 0}>
             <div class="exterior"></div>
             <div class="bg-light w-full p-a-xs">
@@ -100,7 +107,7 @@
         </div>
 
 
-        <div class="col-6">
+        <div class="col-6 col-s-12">
             <div class="h-full w-full " class:active="{tab === 1}" on:click={() => tab = 1}>
             <div class="interior"></div>
             <div class="bg-light w-full p-a-xs">
@@ -109,7 +116,7 @@
             </div>
         </div>
 
-        <div class="col-6">
+        <div class="col-6 col-s-12">
             <div class=" w-full h-full" class:active="{tab === 2}" on:click={() => tab = 2}>
             <div class="village"></div>
             <div class="bg-light w-full p-a-xs">
@@ -118,7 +125,7 @@
             </div>
         </div>
 
-        <div class="col-6">
+        <div class="col-6 col-s-12">
             <div class=" w-full h-full" class:active="{tab === 3}" on:click={() => tab = 3}>
             <div class="details"></div>
             <div class="bg-light w-full p-a-xs">
@@ -127,7 +134,7 @@
             </div>
         </div>
 
-        <div class="col-6">
+        <div class="col-6 col-s-12">
             <div class=" w-full h-full" class:active="{tab === 4}" on:click={() => tab = 4}>
             <div class="nature"></div>
             <div class="bg-light w-full p-a-xs">
@@ -136,7 +143,7 @@
         </div>
         </div>
 
-        <div class="col-6">
+        <div class="col-6 col-s-12">
             <div class=" w-full h-full" class:active="{tab === 5}" on:click={() => tab = 5}>
             <div class="photos"></div>
             <div class="bg-light w-full p-a-xs">
@@ -146,23 +153,28 @@
         </div>
     </div>
      <div class="col-12">
+         <button class="album-open w-full show-xs btn bg-accent m-b-m"on:click={albumsMenu}>Choose gallery</button>
                         {#if tab === 0}
                         <div class="exterior-container">
-                            <div class="grid">
-                                <div class="col-3 exterior-image">
+                            <div class="grid jc-start">
+                                <div class="col-3 col-s-6">
+                                <div class="exterior-image w-full h-full"></div>
+                                </div>
+                                <div class="col-3 col-s-6">
+                                <div class="exterior-image w-full h-full"></div>
                                 </div>
                             </div>
                         </div>
                         {:else if tab === 1}
                         <div class="interior-container">
-                            <div class="grid">
+                            <div class="grid jc-start">
                                 <div class="col-3 exterior-image">
                                 </div>
                             </div>
                         </div>
                         {:else if tab === 2}
                         <div class="interior-container">
-                            <div class="grid">
+                            <div class="grid jc-start">
                                 <div class="col-3">
                                     <img src="assets/images/village.jpg" alt="exterior" class="w-full">
                                 </div>
@@ -170,7 +182,7 @@
                         </div>
                         {:else if tab === 3}
                         <div class="details-container">
-                            <div class="grid">
+                            <div class="grid jc-start">
                                 <div class="col-3">
                                     <img src="assets/images/details.jpg" alt="exterior" class="w-full">
                                 </div>
@@ -178,7 +190,7 @@
                         </div>
                         {:else if tab === 4}
                         <div class="nature-container">
-                            <div class="grid">
+                            <div class="grid jc-start">
                                 <div class="col-3">
                                     <img src="assets/images/interior.jpg" alt="exterior" class="w-full">
                                 </div>
@@ -186,7 +198,7 @@
                         </div>
                             {:else if tab === 5}
                             <div class="guest-container">
-                                <div class="grid">
+                                <div class="grid jc-start">
                                     <div class="col-3">
                                         <img src="assets/images/indulge.jpg" alt="exterior" class="w-full">
                                     </div>
